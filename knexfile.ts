a4b86815166d5c6faa8736ab796ea60113a8ce54
Mikael Messias <mikaelmessias@outlook.com>
2020-06-22 20:38:52 -0300

    Refactor path.resolve
    Signed-off-by: Mikael Messias <mikaelmessias@outlook.com>

--- knexfile.ts
+++ knexfile.ts
@@ -1,15 +1,19 @@
 import path from 'path';
 
+const connectionPath = ['src', 'database', 'database.sqlite'];
+const migrationsPath = ['src', 'database', 'migrations'];
+const seedsPath = ['src', 'database', 'seeds'];
+
 module.exports = {
   client: 'sqlite3',
   connection: {
-    filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite'),
+    filename: path.resolve(__dirname, ...connectionPath),
   },
   migrations: {
-    directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
+    directory: path.resolve(__dirname, ...migrationsPath),
   },
   seeds: {
-    directory: path.resolve(__dirname, 'src', 'database', 'seeds'),
+    directory: path.resolve(__dirname, ...seedsPath),
   },
   useNullAsDefault: true,
 };
--- src/utils/multer.ts
+++ src/utils/multer.ts
@@ -2,9 +2,11 @@ import crypto from 'crypto';
 import multer from 'multer';
 import path from 'path';
 
+const uploadsPath = ['..', '..', 'uploads'];
+
 export default {
   storage: multer.diskStorage({
-    destination: path.resolve(__dirname, '..', '..', 'uploads'),
+    destination: path.resolve(__dirname, ...uploadsPath),
     filename: (request, file, callback) => {
       const hash = crypto.randomBytes(6).toString('hex');
 
